4c6a54d3552a5a2d97d43e30efd587fdf933a2df
kukinisVO <ronaldofernandez1@upb.edu>
2025-05-01 21:07:48 -0400

    Adds Patient model
    Introduces the Patient model with properties for CI, name, and a randomly assigned blood type.

---
+++ ClinicLogic/Models/Patient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicLogic.Models
+{
+    internal class Patient
+    {
+        string[] bloodType = new string[] { "A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-" };
+        public int CI { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BloodType { get; set; }
+        public Patient(int ci, string firstName, string lastName)
+        {
+            CI = ci;
+            FirstName = firstName;
+            LastName = lastName;
+            BloodType = RandomBloodType();
+        }
+
+        // put randomized blood type
+        public string RandomBloodType()
+        {
+            Random random = new Random();
+            int index = random.Next(bloodType.Length);
+            return bloodType[index];
+        }
+    }
+}
